aa2e4618294c49025d640104726692cb34434c27
NihaallX <31230730@vupune.ac.in>
2026-04-29 20:07:46 +0530

    Dampen brand logo exposure thresholds appropriately

--- components/drunkva/ShareOverlay/TemplateC.tsx
+++ components/drunkva/ShareOverlay/TemplateC.tsx
@@ -158,7 +158,7 @@ export function TemplateC({ session, drinks, fastestBeerIsPR }: TemplateCProps) 
           alt="Drunkva"
           width={80}
           height={16}
-          className="h-4 w-auto object-contain opacity-100"
+          className="h-4 w-auto object-contain opacity-60"
         />
       </div>
     </div>
